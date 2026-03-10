1346e476bcd5f7bf614bf4163ff256bd36ee0f83
Thufaill <javierrthufaill@gmail.com>
2026-03-10 11:02:49 +0700

    Percobaan 1 (5.2)

---
+++ Jobsheet5/MainFaktorial18.java
@@ -0,0 +1,17 @@
+package Jobsheet5;
+
+import java.util.Scanner;
+
+public class MainFaktorial18 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai: ");
+        int nilai = input.nextInt();
+
+        Faktorial18 fk = new Faktorial18();
+        System.out.println("Nilai faktorial " + nilai + " menggunakan BF: " + fk.faktorialBF(nilai));
+        System.out.println("Nilai faktorial " + nilai + " menggunakan DC: " + fk.faktorialDC(nilai));
+        input.close();
+    }
+}
